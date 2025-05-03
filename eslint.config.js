096dfb0b8496ecaf299854355ddf77a3df433d30
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2025-05-03 13:52:00 -0500

    Fix ESLint config to use CommonJS syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,20 @@
+// eslint.config.js
 const { defineConfig } = require("eslint-define-config");
 
-module.exports = defineConfig({
-  overrides: [
-    {
-      files: ["*.js", "*.jsx"],
-      languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        globals: {
-          node: "readonly",
-          browser: "readonly",
-        },
+module.exports = defineConfig([
+  {
+    files: ["*.js", "*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        node: "readonly",
+        browser: "readonly",
       },
-      rules: {
-        "react/prop-types": "off", // Example rule modification
-      },
-      plugins: ["react"],
     },
-  ],
-});
+    rules: {
+      "react/prop-types": "off", // Example rule modification
+    },
+    plugins: ["react"],
+  },
+]);
