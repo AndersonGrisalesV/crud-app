19da9a63499daf373c1d9eb07b16d65254bd6dcc
AndersonGrisalesV <grisalesvargasanderson@gmail.com>
2025-05-03 14:05:48 -0500

    Fix ESLint config to use CommonJS syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
 
 export default defineConfig([
-  {
-    parser: "@babel/eslint-parser", // Use Babel ESLint parser
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      ecmaFeatures: {
-        jsx: true, // Enable JSX syntax
-      },
-    },
-    plugins: ["react"], // React plugin for JSX
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    rules: {
-      "react/prop-types": "off", // Disable prop-types rule if not needed
-    },
-    files: ["src/**/*.js"], // Apply to JavaScript files in src
-  },
+  { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
 ]);
